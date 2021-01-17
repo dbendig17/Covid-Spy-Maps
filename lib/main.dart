3a3cb1d15b0eff4b9cce842ba73157350d5b852e
dbendig <dbendig@uoguelph.ca>
2021-01-16 20:28:43 -0500

    updated pin description format

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
   runApp(MyApp());
 }
 
-//TODO: add icon type property, increase severity range from 1-5, incorporate risk hours/days into marker description
+//TODO: add icon type property(dependent on risk type)
 
 class MyApp extends StatefulWidget {
   @override
@@ -13,6 +13,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  var riskDescriptions = {1 :'Large Group: ', 2 :'No Masks', 3 : 'Other: '};
   //list of temp markers for user choice
   List <Marker> myMarker = [];
   //list of all created markers
@@ -20,7 +21,12 @@ class _MyAppState extends State<MyApp> {
   //marker properties
   var markerCount = 0;
   var descriptionInput = 'example description';
+  //int from 1-5
   var severity = 1;
+  var riskHours = '4pm';
+  var riskDays = 'January 16th';
+  //
+  var riskType = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +52,8 @@ class _MyAppState extends State<MyApp> {
   _handleTap(LatLng tappedPoint) {
     markerCount += 1;
     var markerHue = _setHueColor();
+    var risk = riskDescriptions[riskType];
+    var description = riskHours + ' on ' + riskDays + ' : ' + descriptionInput;
     LatLng pos = tappedPoint;
     print(pos);
     setState(() {
@@ -58,8 +66,8 @@ class _MyAppState extends State<MyApp> {
           print(pos);
         },
         infoWindow: InfoWindow(
-          title: 'Test Title',
-          snippet: descriptionInput,
+          title: risk,
+          snippet: description,
         ),
         position: pos,
         icon: markerHue,
